20c0475402ef8c2ab8e6013467b57cd3d0162abc
Dinesh Golla <dinesh.golla@e2open.com>
2024-10-23 10:16:18 +0530

    This Commit is of Java Repo

---
+++ DataBase/dbConnectio .java
@@ -0,0 +1,21 @@
+import java.sql.*;
+class dbConnection{
+    public static void main(String args[]) throws Exception{
+        String sql = "select * from students";
+        String url = "jdbc:mysql://localhost:3306/dineshdb";
+        String userName = "root";
+        String password ="Dinesh@123";
+
+        Connection con = DriverManager.getConnection(url, userName, password);
+
+        Statement st = con.createStatement();
+        ResultSet rs = st.executeQuery(sql);
+        // rs.next();
+        while(rs.next())
+        {
+            System.out.println("Student ID :"+ rs.getInt(1)+" "+"Student Name :"+rs.getString(2)+"    "+"Student Age :"+rs.getInt(3));
+        }
+        con.close();
+
+    }
+}
